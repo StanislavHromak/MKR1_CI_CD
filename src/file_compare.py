5dc5a493980e2f4233307888042252bbdf863a83
Macksym-Savchenko <macksymsnow@gmail.com>
2025-03-28 14:07:32 +0200

    add function find_common_lines

--- src/file_compare.py
+++ src/file_compare.py
@@ -5,3 +5,9 @@ def read_file(file_path):
             return set(line.strip() for line in file if line.strip())
     except FileNotFoundError:
         return set()
+
+def find_common_lines(file1_path, file2_path):
+    """Знаходить рядки в обох файлах."""
+    lines1 = read_file(file1_path)
+    lines2 = read_file(file2_path)
+    return lines1.intersection(lines2)
